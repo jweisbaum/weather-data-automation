2dca0a7179f74af7baff9b3b592d414466ed270c
Jon Weisbaum <jweisbaum89@gmail.com>
2019-10-25 15:02:45 -0700

    Now we layer ascat pngs into a single transparent png

--- ascat.py
+++ ascat.py
@@ -1,45 +1,34 @@
 import wget
 import os
+import subprocess
 
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBas50.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBas51.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBas38.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBas39.png")
+grids = ['50']
+ascat_urls = ['https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMB',
+  'https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMB',
+  'https://manati.star.nesdis.noaa.gov/ascat_images/cur_25km_METC/zooms/WMB']
+as_des = ['as','ds']
 
-os.rename("WMBas50.png", "MetopA-As-WMBas50.png")
-os.rename("WMBas51.png", "MetopA-As-WMBas51.png")
-os.rename("WMBas38.png", "MetopA-As-WMBas38.png")
-os.rename("WMBas39.png", "MetopA-As-WMBas39.png")
+newname = ''
+oldname = ''
+for index in grids:
+    filenumber = 0;
+    for url in ascat_urls:
+        for direction in as_des:
+            to_use = url + direction + index +'.png'
+            wget.download(to_use)
+            newname = index + '_'  + str(filenumber) + '.png'
+            oldname = 'WMB' + direction + index + '.png'
+            os.system('convert ' + oldname + ' -transparent white ' + newname)
+            os.remove(oldname)
+            if(filenumber > 0):
+                previous = index + '_'  + str(filenumber-1) + '.png'
+                os.system('convert ' + previous + ' ' + newname + ' -gravity center -compose over -composite temp.png')
+                os.remove(previous)
+                os.remove(newname)
+                os.rename('temp.png', newname)
+            filenumber+=1
+    os.rename(newname, index + '_composite.png')
+          
+ 
 
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBds50.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBds51.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBds38.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopA/WINDS/cur_25km/zooms/WMBds39.png")
-os.rename("WMBds50.png", "MetopA-Ds-WMBas50.png")
-os.rename("WMBds51.png", "MetopA-Ds-WMBas51.png")
-os.rename("WMBds38.png", "MetopA-Ds-WMBas38.png")
-os.rename("WMBds39.png", "MetopA-Ds-WMBas39.png")
-
-
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBas50.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBas51.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBas38.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBas39.png")
-
-os.rename("WMBas50.png", "MetopB-As-WMBas50.png")
-os.rename("WMBas51.png", "MetopB-As-WMBas51.png")
-os.rename("WMBas38.png", "MetopB-As-WMBas38.png")
-os.rename("WMBas39.png", "MetopB-As-WMBas39.png")
-
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBds50.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBds51.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBds38.png")
-wget.download("https://www.ospo.noaa.gov/data/atmosphere/ascat/MetopB/WINDS/cur_25km/zooms/WMBds39.png")
-
-os.rename("WMBds50.png", "MetopB-Ds-WMBas50.png")
-os.rename("WMBds51.png", "MetopB-Ds-WMBas51.png")
-os.rename("WMBds38.png", "MetopB-Ds-WMBas38.png")
-os.rename("WMBds39.png", "MetopB-Ds-WMBas39.png")
-
-
-wget.download("https://ftp.opc.ncep.noaa.gov/data/ascat_ab/latest_ascat_swath_data.grb2")
+#wget.download("https://ftp.opc.ncep.noaa.gov/data/ascat_ab/latest_ascat_swath_data.grb2")
